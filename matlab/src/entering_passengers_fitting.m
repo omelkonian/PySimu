2492b6f0be9565feb15fbeecd0c543d7d35451f9
caro90 <aronis.ch@gmail.com>
2017-10-21 22:41:16 +0200

    Add files via upload

--- matlab/src/entering_passengers_fitting.m
+++ matlab/src/entering_passengers_fitting.m
@@ -31,21 +31,31 @@ clear X data;
 %% One lambda for every station every 15 minutes
 i=1;
 temp=1;
+
+timing=timing_AZU_CS_sorted;
+direction=AZU_CS_entering_sorted;
+
 for j=1:9
-    while i<(length(entering_global) )
-       X=entering_global(i,j);
-       current_time=timing_entering_global(i);
+    time=timing(1)-minutes(10);
+    while i<(length(direction) )
+       X=direction(i,j);
+       current_time=timing(i);
        k=i+1;
-       while (timing_entering_global(k) <= current_time + minutes(15) ...
-               && k<length(entering_global))
+       
         
-              X=[X;entering_global(k,j)];
+       while (timing(k) <=  time + minutes(15) ...
+               && k<length(direction))
+        
+              X=[X;direction(k,j)];
               k=k+1;
        end
+       % lambdahat = poissfit(data) returns the maximum likelihood estimate 
+       % (MLE) of the parameter of the Poisson distribution, ?, given the data data.
        passengers_entering_lambdas(temp,j)=[poissfit(X)];
        temp=temp+1;
        i=k;
        clear X;
+       time=time+minutes(15);
     end
     i=1;
     temp=1;
--- matlab/src/leaving_passengers_fitting.m
+++ matlab/src/leaving_passengers_fitting.m
@@ -1,44 +1,82 @@
-%% leaving passengers data fitting
-clear X;
+%% Leaving passengers data fitting
 %% One lambda for every station every 15 minutes
-
 %% **** Beta can't fit the data if all values are the same. 
-%% ****problem with last column where everything is 1
+%% **** problem with last column where everything is 1
+% i=1;
+% temp=1;
+% alpha=0.05;
+% 
+% timing=timing_AZU_CS_sorted;
+% direction=AZU_CS_leaving_percen_sorted;
+% 
+% %transform the direction to have values between [0 1]
+% direction=direction./100;
+% 
+% for j=1:9
+%     time=timing(1)-minutes(10);
+%     while i<(length(direction) )
+%        X=direction(i,j);
+%        current_time=timing(i);
+%        k=i+1;
+%        while (timing(k) <=  time + minutes(15) ...
+%                && k<length(direction))
+%         
+%               X=[X;direction(k,j)];
+%               k=k+1;
+%        end
+%        %beta can not be fitted if all data values are the same
+%        if sum(X(:))==0
+%            %beta can not be fitted if all data values are the same
+%            passengers_leaving_parameter_a(temp,j)=0;
+%            passengers_leaving_parameter_b(temp,j)=0;
+%       
+%        else
+%            [phat,pci] = betafit(X,alpha);
+%            passengers_leaving_parameter_a(temp,j)=[phat(1)];
+%            passengers_leaving_parameter_b(temp,j)=[phat(2)];
+%        end
+%        
+%        time=time+minutes(15);
+%        temp=temp+1;
+%        i=k;
+%        clear X;
+%     end
+%     i=1;
+%     temp=1;
+% end
+% 
+% clear i temp time  alpha;
 
-%transfom of leaving_global_percen to have values between [0 1]
-leaving_global_percen2=leaving_global_percen./100;
 
-i=1;
-temp=1;
-alpha=0.05;
-for j=1:9
-    while i<(length(leaving_global) )
-       X=leaving_global_percen2(i,j);
-       current_time=timing_leaving_global(i);
-       k=i+1;
-       while (timing_leaving_global(k) <= current_time + minutes(15) ...
-               && k<length(leaving_global))
-        
-              X=[X;leaving_global_percen2(k,j)];
-              k=k+1;
-       end
-       %beta can not be fitted if all data values are the same
-       if sum(X(:))==0
-           %beta can not be fitted if all data values are the same
-           passengers_leaving_parameters_a(temp,j)=0;
-           passengers_leaving_parameters_b(temp,j)=0;
-      % elseif (X)
-       else
-           [phat,pci] = betafit(X,alpha);
-           passengers_leaving_parameter_a(temp,j)=[phat(1)];
-           passengers_leaving_parameter_b(temp,j)=[phat(2)];
-       end
-       
-       
-       temp=temp+1;
-       i=k;
-       clear X;
+%% Leaving passengers fitting for every station for all hours for all days
+% and 
+% transformations into percentages
+
+direction_leaving=CS_AZU_leaving;
+direction_entering=CS_AZU_entering;
+
+C=cell(9,1);
+for i=1:length(direction_leaving)
+    sum = 0;
+    for j=1:9
+        sum=sum+direction_entering(i,j);
+        if ne(sum,0)
+           p=(direction_leaving(i,j)/sum);
+           C{j}=[C{j} p];
+           sum=sum-direction_leaving(i,j);
+        end
     end
-    i=1;
-    temp=1;
 end
+exit_percentage=zeros(9,2);
+for j=2:8
+    [PHAT, PCI] = betafit(C{j}, 0.05);
+    exit_percentage(j,:) = [PHAT(1), PHAT(2)]; 
+end
+
+clear i j p PHAT PCI sum;
+
+%% code to save and concat after the above fitting
+%% execute in command line
+% AZU_CS_leaving_parameters=exit_percentage;
+% CS_AZU_leaving_parameters=exit_percentage;
+% leaving_parameters=[AZU_CS_leaving_parameters;CS_AZU_leaving_parameters];
